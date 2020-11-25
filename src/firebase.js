06334b078a294ba11113a72aaf1ce908d6a2a069
cawauchi6204 <cawauchi6204@gmail.com>
2020-11-25 21:26:17 +0900

    remove hard coded env vars

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID
+};
+
+firebase.initializeApp(firebaseConfig)
+const database = firebase.database()
+const messagesRef = database.ref('messages')
+
+export const pushMessage = ({ name, text }) => {
+  messagesRef.push({ name, text })
+}
