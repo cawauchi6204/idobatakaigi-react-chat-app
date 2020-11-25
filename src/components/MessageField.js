d8e89aa3a95a1f245dbad6849f84d14db567206c
cawauchi6204 <cawauchi6204@gmail.com>
2020-11-25 20:58:24 +0900

    create-app

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import { TextField } from '@material-ui/core'
 
+import { pushMessage } from '../firebase'
+
 const MessageField = ({ name, setText, text }) => {
-  console.log({ text })
   const [isComposed, setIsComposed] = useState(false)
 
   return <TextField
@@ -13,7 +14,7 @@ const MessageField = ({ name, setText, text }) => {
       const text = e.target.value
       if (text === '') return
       if (e.key === 'Enter') {
-        console.log('push message to firebase')
+        pushMessage({ name: 'ハムさん', text })
         setText('')
         e.preventDefault()
       }
